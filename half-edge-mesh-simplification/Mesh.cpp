1cd5586bf79f460607216a4b531bbced4d816f70
rodsonchue <warboi@hotmail.com>
2016-02-16 09:51:33 +0800

    initial commit

---
+++ half-edge-mesh-simplification/Mesh.cpp
@@ -0,0 +1,82 @@
+#include "Mesh.h"
+
+Mesh::Mesh(const char* filename){	
+	loadMF(filename);
+}
+
+void Mesh::loadMF(const char* filename){
+	if(V.size()>0) V.clear();
+	if(F.size()>0) F.clear();
+	std::ifstream infile;
+	infile.open(filename, std::ios::in);
+	std::string strbuff;
+	while(std::getline(infile,strbuff)){
+		std::stringstream ss;
+		ss<<strbuff;
+		char type;
+		ss>>type;
+		if(type=='v'){
+			Vertex v;
+			ss>>v.x>>v.y>>v.z;
+			V.push_back(v);
+		}
+		else if(type=='f'){
+			Face f;
+			ss>>f.a>>f.b>>f.c;
+			F.push_back(f);
+		}
+	}
+	infile.close();
+}
+
+void Mesh::writeMF(const char* filename){
+	std::ofstream outfile;
+	outfile.open(filename, std::ios::out);
+	std::string strbuff;
+	for(uint i=0;i<V.size();i++){
+		outfile<<"v "<<V[i].x<<" "<<V[i].y<<" "<<V[i].z<<std::endl;
+	}
+	for(uint i=0;i<F.size();i++){
+		outfile<<"f "<<F[i].a<<" "<<F[i].b<<" "<<F[i].c<<std::endl;
+	}
+	outfile.close();
+}
+
+void Mesh::simplifyMesh(const char* input, const char* output, int faceCnt){
+	// you may assume inputs are always valid
+	loadMF(input);
+	convertMesh();
+	std::cout<<"Original face count: "<<HEF.size()<<std::endl;
+	// do mesh simplification here
+
+	revertMesh();
+	writeMF();
+}
+
+void Mesh::convertMesh()
+{
+}
+
+void Mesh::revertMesh()
+{
+}
+
+std::vector<HEVertex> Mesh::neighborVertices(HEVertex v)
+{
+}
+
+std::vector<HEFace> Mesh::neighborFaces(HEVertex v)
+{
+}
+
+std::vector<HEVertex> Mesh::adjacentVertices(HEFace f)
+{
+}
+
+int Mesh::Vcnt(){
+	return HEV.size();
+}
+
+int Mesh::Fcnt(){
+	return HEF.size();
+}
